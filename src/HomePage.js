558defbd7c55d0af272c130699c6b187c85575b1
nakulgoel55 <nakulgoel55@gmail.com>
2023-02-21 00:38:13 -0500

    ic

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,9 +3,16 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className="home-page">
-      <video autoPlay={true} loop muted playsInline className="video-background">
-        <source src="videos/10.mp4" type="video/mp4" />
-      </video>
+      <video className='video-background'
+            src="videos/10.mp4"
+            controls
+            muted
+            autoPlay={"autoplay"}
+            preLoad="auto"
+            loop 
+ > </video>
+
+
       <div className="text-overlay">
         <h1>Welcome to Version Infinity</h1>
       </div>

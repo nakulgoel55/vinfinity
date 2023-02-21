09ba656e3209370b14401f948d9f421c43a3d009
nakulgoel55 <nakulgoel55@gmail.com>
2023-02-21 00:30:54 -0500

    next

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,16 @@
-import React from 'reac'
+import React from 'react'
 
-const homePage = () => {
-    return(
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <video autoPlay loop muted className="video-background">
+        <source src="videos/10.mp4" type="video/mp4" />
+      </video>
+      <div className="text-overlay">
+        <h1>Welcome to Version Infinity</h1>
+      </div>
+    </div>
+  );
+};
 
-        <div className="home-page">
-            <video autoPlay muted loop playsInline>
-                <source src="videos/10.mp4" type="video/mp4" />
-            </video>
-            <div className="text-overlay">
-                <h1>Welcome to Version Infinity</h1>
-            </div>
-            </div>
-        );
-    };
-
-    )
-}
+export default HomePage;

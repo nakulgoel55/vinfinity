7c8f97790cc5b251e61a6492058b74ebb3362a63
Zara Goel <zaragoel@Zaras-MacBook-Pro.local>
2023-02-21 00:07:14 -0500

    Initial Commit

---
+++ src/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomePage = () => {
+    return(
+
+        <div className="home-page">
+            <video autoPlay loop muted className="video-background">
+                <source src="/videos/10.mp4" type="video/mp4" />
+            </video>
+            <div className="text-overlay">
+                <h1>Welcome to Version Infinity</h1>
+            </div>
+            </div>
+        );
+    };
+
+export default HomePage;
